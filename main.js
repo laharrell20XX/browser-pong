45dc829b7e3b374cf52586933769f9c403c78de5
laharrell20XX <lharrell@basecampcodingacademy.org>
2018-10-24 09:49:54 -0500

    cleared the entire file, and restarted using canvas animation instead of jquery
    Additions:
    initialization (global variables):
    added the ball's initial position on the x axis and on the y axis (bx, by); added variable moveRight and set it's initial value to be true; added variable tickRate and set its inital value to be 1000
    
    window.onload:
    set the value to be a function, that when called, gets the canvas element and its context from the page, draws the ball at its initial position and calls setInterval(pong as its handler, and tickRate / 60 as the interval)
    
    pong():
    gets the canvas element and its context from the file; clears the entire canvas (clearRect); first test checks to see if moveRight is true and bx is less than the canvas.width - 10; second part helps the ball not leave the canvas, instead just hitting close to the edge of it; if true, bx is increased by 5 and a new rectangle is drawn at that new bx (fillRect); if false, ball has hit the edge of the screen and moveRight is set to false; this keeps the first test from passing; bx is decreased by 5 and the canvas is redrawn until the ball has hit the left edge of the canvas; when this happens, moveRight is set to true meaning the first test can pass again

--- main.js
+++ main.js
@@ -1,16 +1,37 @@
-const H = 600;
+// if (bx + 5 > canvas.width) {
+//     while (bx > 10) { // wraps the ball around the screen
+//         canvasCtx.clearRect(0, 0, 1192, 600);
+//         bx -= 5;
+//         canvasCtx.fillRect(bx, by, 10, 10);
+//     }
+// }
+var bx = 596; // ball's pos on the x axis
+var by = 300; // ball's pos on the y axis
+var moveRight = true; // state of movement on the ball, if true, ball is moving right; if false ball is moving left
+var tickRate = 1000;
 
-function moveBallDown() {
-    var ball = $(".ball");
-    ball.animate({ marginTop: "590px" }, { duration: 1000, easing: "linear" });
+window.onload = function() {
+    var canvas = this.document.querySelector("canvas.world");
+    var canvasCtx = canvas.getContext("2d");
+    canvasCtx.fillRect(by, bx, 10, 10);
+    this.setInterval(pong, tickRate / 60); // 30 times per second
+};
+
+function pong() {
+    canvas = this.document.querySelector("canvas.world");
+    var canvasCtx = canvas.getContext("2d");
+    console.log(bx);
+    canvasCtx.clearRect(0, 0, 1192, 600);
+    if (moveRight && bx < canvas.width - 10) {
+        bx += 5;
+        canvasCtx.fillRect(bx, by, 10, 10);
+    } else {
+        moveRight = false; // keeps the ball from moving right
+        bx -= 5;
+        if (bx - 5 < 0) {
+            // ball's x is less than the canvas size
+            moveRight = true; // ball moves right again
+        }
+        canvasCtx.fillRect(bx, by, 10, 10);
+    }
 }
-function moveBallUp() {
-    $(".ball").animate(
-        { marginTop: "0px" },
-        { duration: 1000, easing: "linear" }
-    );
-}
-setInterval(() => {
-    moveBallDown();
-    moveBallUp();
-}, 1000);
