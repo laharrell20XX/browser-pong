4e79c5d1751f25567a9b6f7f5404cbbc65bb5069
laharrell20XX <lharrell@basecampcodingacademy.org>
2018-10-24 11:18:58 -0500

    added a window event that listens for a keypress; listener calls function movePaddle which allows the paddle to move
    initialization:
    added a new variable px that represents the position of the paddle on the x axis; added variable py that sets the initial position of the paddle on the y axis (middle of the paddle starts at the middle of the screen); created variable paddleHeight that sets the initial height of the paddle
    
    additions/changes:
    
    window.onload:
    called fillRect that adds the paddle to the canvas (will replace x, and y and height with initial values); middle of paddle starts at the middle of the screen, 10 points from the edge of the canvas, and the paddle is 25% less than the height of the canvas; added an event listener to the window where the event is a keypress and the listener is a function that calls movePaddle with the keypress event passed
    
    movePaddle:
    function that takes in a keyboard event, and moves the paddle up or down; gets the canvas and its context so it can be drawn and redrawn; mainly uses a switch whose test expression is the key value pressed at the event ev.key; it has two cases: one for where the ev.key evaluates to "ArrowDown" (down arrow pressed) and one where ev.key evaluates to "ArrowUp"; in each case, there is one if else statement; the one in "ArrowDown" checks the paddle to see if the bottom of it touches the canvas; if true the function does nothing, otherwise the paddle's previous position is cleared before py is increased by 10 and the paddle is redrawn; the "ArrowUp" tests to see if the top of the paddle touches the top of the canvas; if true nothing happens, otherwise the paddle is cleared at its previous position, the py is decreased by 10, and the paddle is redrawn;
    
    pong:
    changed the initial canvas clear to just clear the ball's previous position not the entire screen (...(0, 0, 10, 10) changed to ...(bx, by, 10, 10))

--- main.js
+++ main.js
@@ -7,6 +7,9 @@
 // }
 var bx = 596; // ball's pos on the x axis
 var by = 300; // ball's pos on the y axis
+const px = 1182;
+var py = 300 - 600 * 0.1;
+var paddleHeight = 600 * 0.25;
 var moveRight = true; // state of movement on the ball, if true, ball is moving right; if false ball is moving left
 var tickRate = 1000;
 
@@ -14,14 +17,22 @@ window.onload = function() {
     var canvas = this.document.querySelector("canvas.world");
     var canvasCtx = canvas.getContext("2d");
     canvasCtx.fillRect(by, bx, 10, 10);
+    canvasCtx.fillRect(
+        1182,
+        300 - canvas.height * 0.1,
+        10,
+        canvas.height * 0.25
+    );
+    window.addEventListener("keypress", function(ev) {
+        movePaddle(ev);
+    });
     this.setInterval(pong, tickRate / 60); // 30 times per second
 };
 
 function pong() {
     canvas = this.document.querySelector("canvas.world");
     var canvasCtx = canvas.getContext("2d");
-    console.log(bx);
-    canvasCtx.clearRect(0, 0, 1192, 600);
+    canvasCtx.clearRect(bx, by, 10, 10);
     if (moveRight && bx < canvas.width - 10) {
         bx += 5;
         canvasCtx.fillRect(bx, by, 10, 10);
@@ -33,5 +44,30 @@ function pong() {
             moveRight = true; // ball moves right again
         }
         canvasCtx.fillRect(bx, by, 10, 10);
+    }
+}
+
+function movePaddle(ev) {
+    canvas = this.document.querySelector("canvas.world");
+    var canvasCtx = canvas.getContext("2d");
+    switch (ev.key) {
+        case "ArrowDown":
+            if (py >= canvas.height - paddleHeight) {
+                break;
+            } else {
+                canvasCtx.clearRect(px, py, 10, paddleHeight);
+                py += 10;
+                canvasCtx.fillRect(px, py, 10, paddleHeight);
+                break;
+            }
+        case "ArrowUp":
+            if (py <= 0) {
+                break;
+            } else {
+                canvasCtx.clearRect(px, py, 10, paddleHeight);
+                py -= 10;
+                canvasCtx.fillRect(px, py, 10, paddleHeight);
+                break;
+            }
     }
 }
